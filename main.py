2688d77ba096aa5993185578e93af64f3a9e0601
ztz <1558683684@qq.com>
2026-07-07 19:40:01 +0800

    测试-ztz

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi 百霖123, {name}')  # 按 Ctrl+F8 切换断点。
 
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi 铁昭123, {name}')  # 按 Ctrl+F8 切换断点。
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
